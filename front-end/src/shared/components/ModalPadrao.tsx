9d200c19ec0df43145345a213674db5200fbe6a5
Gustavo Ribeiro <gustavo.ribeiro@dtidigital.com.br>
2022-04-10 19:22:48 -0300

    Finaliza tela inicio

--- front-end/src/shared/components/ModalPadrao.tsx
+++ front-end/src/shared/components/ModalPadrao.tsx
@@ -1,40 +1,42 @@
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import { ReactNode, MouseEventHandler } from "react";
 
-export default function CustomizedDialogs(props: any) {
-    const { children, onClose,  open} = props;
+type ModalPadraoProps = {
+  children: ReactNode;
+  onClose: MouseEventHandler<HTMLAnchorElement | HTMLButtonElement>;
+  open: boolean;
+};
+
+export function ModalPadrao(props: ModalPadraoProps) {
+  const { children, onClose, open } = props;
 
   return (
     <div>
-      <Dialog
-        onClose={onClose}
-        open={open}
-      >
+      <Dialog onClose={onClose} open={open}>
         <IconButton
           aria-label="close"
           onClick={onClose}
           sx={{
-            position: 'absolute',
+            position: "absolute",
             right: 8,
             top: 8,
-            background: '#1793A6',
+            background: "#1793A6",
             borderEndStartRadius: 10,
-            borderBlockColor: '#1793A6',
+            borderBlockColor: "#1793A6",
             height: 20,
             width: 20,
-            color: '#fff',
+            color: "#fff",
             ":hover": {
-                color: '#1793A6',
-            }
+              color: "#1793A6",
+            },
           }}
         >
-            <CloseIcon fontSize='small'/>
+          <CloseIcon fontSize="small" />
         </IconButton>
-        <DialogContent style={ { background: "#fff" }}>
-            {children}
-        </DialogContent>
+        <DialogContent style={{ background: "#fff" }}>{children}</DialogContent>
       </Dialog>
     </div>
   );
